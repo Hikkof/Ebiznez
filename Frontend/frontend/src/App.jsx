70cbb8b087bf4ac68dc9a6a1b29001fd66e593db
Hikkof <108770694+Hikkof@users.noreply.github.com>
2025-08-28 21:55:12 +0200

    Add files via upload

---
+++ Frontend/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { Products } from './components/Products.jsx';
+import { Payments } from './components/Payments.jsx';
+
+function App() {
+    return (
+        <div>
+            <Products/>
+            <Payments/>
+        </div>
+    );
+}
+
+export default App;
